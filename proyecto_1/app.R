f2db8a5779de3af3aa2d95740477c5ffb53fab26
samuel <samuelhernandezv262@gmail.com>
2024-06-18 21:30:31 -0600

    correccion del selectinput

--- proyecto_1/app.R
+++ proyecto_1/app.R
@@ -12,9 +12,9 @@ ui <- fluidPage(
   #Selección por años y botoncito de generar
   sidebarLayout(
     sidebarPanel(
-      selectInput("Año2", "Elige el año de la esperanza de vida:",
-                  choices = unique(gapminder$year)),
       selectInput("Año1", "Elige el año del PIB per cápital:",
+                  choices = unique(gapminder$year)),
+      selectInput("Año2", "Elige el año de la esperanza de vida:",
                   choices = unique(gapminder$year)),
       selectInput("Año3", "Elige el año de la correlación:",
                   choices = unique(gapminder$year)),
